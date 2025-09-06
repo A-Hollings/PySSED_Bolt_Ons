9a8f1e958e4e41605e09dd253773a55960b1cccc
A-Hollings <146093739+A-Hollings@users.noreply.github.com>
2025-09-06 12:36:48 +0100

    Cleaned up old code and removed all redundant parts from development process. Plotting was also duplicated in places so removed that.

--- Experimental_Hover_HR_Plotter.py
+++ Experimental_Hover_HR_Plotter.py
@@ -10,7 +10,6 @@ import base64
 output_df = pd.read_csv('60Filters_r-0.85306_pl-0.2/output.dat',
                         sep = '\t', header = 3, low_memory=False)
 #----------------------------------------------------------------------------------------------
-
 # OLD CODE FOR READING IN DIFFERENT DATA SOURCES
 
 #hrd_df = pd.read_csv('60Filters_r-0.85306_pl-0.2/Only MiniJPAS SEDs/hrd.dat', sep = '\t', header = 1)
@@ -45,37 +44,19 @@ true_distance_df = true_distance_df[true_distance_df['Luminosity'] != '0']
 
 true_distance_df['Teff'] = true_distance_df['Teff'].astype(float)
 true_distance_df['Luminosity'] = true_distance_df['Luminosity'].astype(float)
+# MOVED LOGGING TRUE_DISTANCE_DF['DISTANCE'] TO BEFORE EVOLTUION TYPE SPLIT - ISSUES LATER?
+true_distance_df['Distance'] = true_distance_df['Distance'].astype(float)
+true_distance_df['Distance'] = np.log(true_distance_df['Distance'])
 
-# Dataframes split by evolution stage
+#----------------------------------------------------------------------------------------------
+# SPLITTING INTO DATAFRAMES BY EVOLUTION STAGE IF IT IS WANTED TO SHOW ONLY CERTAIN TYPES OF STELLAR OBJECTS
 
 wd_df = true_distance_df.loc[(true_distance_df['Teff'] > 5300) & (true_distance_df['Teff'] < 30000) & (true_distance_df['Luminosity'] < 0.1)]
 rg_df = true_distance_df.loc[(true_distance_df['Teff'] < 5500) & (true_distance_df['Luminosity'] > 2)]
 ms_df = true_distance_df[~true_distance_df.index.isin(wd_df.index) & ~true_distance_df.index.isin(rg_df.index)]
 
-true_distance_df['Distance'] = true_distance_df['Distance'].astype(float)
-true_distance_df['Distance'] = np.log(true_distance_df['Distance'])
-
-fig = px.scatter(true_distance_df, x='Teff', y='Luminosity', color='Distance',
-                 labels=dict(x='Temperature (K)', y='Luminosity (Solar)', color='Distance (pc)'),
-                 width=1000, height=700,
-                 log_y=True, log_x=True)
-
-fig['layout']['xaxis']['autorange'] = 'reversed'
-fig.update_traces(marker_size=4)
-fig.update_layout(
-    coloraxis_colorbar=dict(tickvals=[np.log(100), np.log(1000), np.log(10000)], ticktext=[100, 1000, 10000],
-                            title='Distance (pc)'))
-# Update the title
-fig.update_layout(title_text='H-R Diagram for Gaia & miniJPAS Data produced by PySSED', title_x=0.5, title_y=0.96)
-
-# Update the axis labels
-fig.update_xaxes(title_text='Effective Temperature (K)')
-fig.update_yaxes(title_text='Luminosity (Solar)')
-
-#fig.show()
-
-true_distance_df['Distance'] = true_distance_df['Distance'].astype(float)
-true_distance_df['Distance_log'] = np.log(true_distance_df['Distance'])
+#----------------------------------------------------------------------------------------------
+# ALLOWING LOCAL IMAGE FILES TO BE USED BY DASH WEBAPP INSTEAD OF ONLINE SOURCES
 
 def encode_image(path):
     if not os.path.exists(path):
@@ -91,56 +72,36 @@ file_extension = ".png"
 
 # Build the full file path for each Object
 true_distance_df["Object_Path"] = true_distance_df["Object"].apply(
-    lambda name: os.path.join(image_directory, str(name) + file_extension)
-)
+    lambda name: os.path.join(image_directory, str(name) + file_extension))
 
 # Encode images to base64 and store in IMG_URL
 true_distance_df["IMG_URL"] = true_distance_df["Object_Path"].apply(encode_image)
 
-df = true_distance_df
+#----------------------------------------------------------------------------------------------
+# SETTING UP THE PLOT IN PLOTLY
 
-#fig = go.Figure(data=[
-    #go.Scatter(
-        #x=df["LOGP"],
-        #y=df["PKA"],
-        #mode="markers",
-        #marker=dict(
-            #colorscale='viridis',
-            #color=df["MW"],
-            #size=df["MW"],
-            #colorbar={"title": "Molecular<br>Weight"},
-            #line={"color": "#444"},
-            #reversescale=True,
-            #sizeref=45,
-            #sizemode="diameter",
-            #opacity=0.8,
-        #)
-    #)
-#])
-
-fig = px.scatter(true_distance_df, x='Teff', y='Luminosity', color='Distance_log',
+fig = px.scatter(true_distance_df, x='Teff', y='Luminosity', color='Distance',
                  labels=dict(x='Temperature (K)', y='Luminosity (Solar)', color='Distance (pc)'),
                  width=1000, height=700,
                  log_y=True, log_x=True)
 
 fig['layout']['xaxis']['autorange'] = 'reversed'
 
-# Update the axis labels
+# UPDATE AXIS LABELS, COLOURBAR AND TITLES
+
 fig.update_xaxes(title_text='Effective Temperature (K)')
 fig.update_yaxes(title_text='Luminosity (☉)')
+fig.update_layout(
+    coloraxis_colorbar=dict(tickvals=[np.log(100), np.log(1000), np.log(10000)], ticktext=[100, 1000, 10000],
+                            title='Distance (pc)'))
+fig.update_layout(title_text='H-R Diagram for Gaia & miniJPAS Data produced by PySSED', title_x=0.5, title_y=0.96)
 
 # turn off native plotly.js hover effects - make sure to use
 # hoverinfo="none" rather than "skip" which also halts events.
 fig.update_traces(hoverinfo="none", hovertemplate=None, marker_size=4)
 
-#fig.update_layout(
-    #xaxis=dict(title='Log P'),
-    #yaxis=dict(title='pkA'),
-    #plot_bgcolor='rgba(255,255,255,0.1)'
-#)
-
-fig.update_layout(coloraxis_colorbar=dict(tickvals=[np.log(100),np.log(1000),np.log(10000)], ticktext=[100, 1000, 10000],title='Distance (pc)'))
-#fig.update_layout(title_text='H-R Diagram for Gaia & miniJPAS Data produced by PySSED',title_x=0.5,title_y=0.96)
+#----------------------------------------------------------------------------------------------
+# VOODOO DASH-MAGIC
 
 app = Dash(__name__)
 
@@ -148,7 +109,6 @@ app.layout = html.Div([
     dcc.Graph(id="graph-basic-2", figure=fig, clear_on_unhover=True),
     dcc.Tooltip(id="graph-tooltip"),
 ])
-
 
 @app.callback(
     Output("graph-tooltip", "show"),
@@ -160,12 +120,12 @@ def display_hover(hoverData):
     if hoverData is None:
         return False, no_update, no_update
 
-    # demo only shows the first point, but other points may also be available
+    # Will only show the first point, but other points may also be available if highly stacked
     pt = hoverData["points"][0]
     bbox = pt["bbox"]
     num = pt["pointNumber"]
 
-    df_row = df.iloc[num]
+    df_row = true_distance_df.iloc[num]
     img_src = df_row['IMG_URL']
     name = df_row['Object']
     temperature = df_row['Teff']
